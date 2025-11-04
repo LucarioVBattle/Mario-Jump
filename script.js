1414f6d2eae467ce893f4673df8b16844485b94d
LucarioVBattle <ttutuman@yahoo.com>
2025-11-04 17:29:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const mario =  document.querySelector('.mario');
+const pipe = document.querySelector ('.pipe')
+const clouds = document.querySelector ('.clouds')
 
+const scoreDisplay = document.getElementByID('score')
+const restartBtn = Document.getElementById('restartBtn')
+
+let score = 0;
+let isGameOver = false;
+
+const jump = () => {
+  if (!isGameOver) {
+    mario.classList.add('jump');
+    setTimeout(() => {
+      mario.classList.remove('jump')
+    }, 500);
+  }
+};
+document.addEventListener("keydown", (event) => {
+  if (event.code === 'space') {
+    jump()
+  }
+});
