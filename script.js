edde2b669323ffb908fefa35fa2be9c33b48ef2b
LucarioVBattle <ttutuman@yahoo.com>
2025-11-19 14:17:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,69 @@
-const mario =  document.querySelector('.mario');
-const pipe = document.querySelector ('.pipe')
-const clouds = document.querySelector ('.clouds')
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
-const scoreDisplay = document.getElementByID('score')
-const restartBtn = Document.getElementById('restartBtn')
+const scoreDisplay = document.getElementById('score');
+const restartBtn = document.getElementById('restartBtn');
 
 let score = 0;
 let isGameOver = false;
 
+// FUNÇÃO DE PULO
 const jump = () => {
   if (!isGameOver) {
     mario.classList.add('jump');
+
     setTimeout(() => {
-      mario.classList.remove('jump')
+      mario.classList.remove('jump');
     }, 500);
   }
 };
+
+// DETECTAR TECLA
 document.addEventListener("keydown", (event) => {
-  if (event.code === 'space') {
-    jump()
+  if (event.code === 'Space') {
+    jump();
   }
 });
+
+// LOOP PRINCIPAL
 const loop = setInterval(() => {
+
   const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).botton.replace('px', '');
-  const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px','');
-  if (pipePosition <= 120 && pipePosition > && marioPosition < 80) {
-  pipe.style.animation = 'none'
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px', '');
+
+  // COLISÃO
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+    // Parar animações
+    pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
-    clouds.style.animation = 'none'
-    clouds.style.right
+
+    clouds.style.animation = 'none';
+    clouds.style.right = `${cloudsPosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = './images/game-over.png';
+    mario.style.width = '75px';
+    mario.style.marginLeft = '40px';
+
+    isGameOver = true;
+
+    clearInterval(loop);
+  } else {
+    // conta pontos enquanto o jogo está rodando
+    if (!isGameOver) {
+      score++;
+      scoreDisplay.textContent = score;
+    }
+  }
+
+}, 10);
+
+// BOTÃO DE RESTART
+restartBtn.addEventListener('click', () => {
+  location.reload();
+});
